ba221580b759c2b60c11409da16f95a7a9500093
Martina Kraus <kraus.martina.m@gmail.com>
2025-03-16 20:53:38 +0100

    new Task for CSRF

--- Task_06/csrf-vulnerable-site/server/server.js
+++ Task_06/csrf-vulnerable-site/server/server.js
@@ -39,11 +39,11 @@ app.get('/api/profile', function (req, res) {
 
 
 app.post('/api/profile', function (req, res) {
-    // const cookie = req.cookies['XSRF-TOKEN'];
-    // if (req.headers['x-xsrf-token'] !== cookie) {
-    //     res.status(403).send({ text: 'Invalid CSRF Token' });
-    //     return;
-    // }
+    // Todo: Implement CSRF protection
+    // Get the cookie: req.cookies['XSRF-TOKEN'];
+    // Get the header: req.headers['x-xsrf-token'];
+    // Compare the cookie and header
+    // If they are not the same, return 403
     
         user.username = req.body.username || user.username,
         user.password = req.body.password || user.password,
--- Task_06/csrf-vulnerable-site/src/app/app.config.ts
+++ Task_06/csrf-vulnerable-site/src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withXsrfConfiguration } from '@angular/common/http';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { routes } from './app.routes';
+import { ApplicationConfig } from "@angular/core";
+import { provideHttpClient, withXsrfConfiguration } from "@angular/common/http";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withXsrfConfiguration({})),
-    provideRouter(routes, withComponentInputBinding())
-  ]
+    // Todo implement xsrf protection (withXsrfConfiguration)
+    provideHttpClient(),
+    provideRouter(routes, withComponentInputBinding()),
+  ],
 };
